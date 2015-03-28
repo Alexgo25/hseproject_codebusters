1fe367ac4847f661821ef4a963c42941ae64722b
Alexgo25 <alexgo25616@gmail.com>
2015-03-28 18:40:49 +0300

    allDetailsAdded

--- Codebusters_sample/GameScene.swift
+++ Codebusters_sample/GameScene.swift
@@ -15,6 +15,9 @@ class GameScene: SKScene {
     var button_tips : SKSpriteNode?
     var button_moveforward : SKSpriteNode?
     var button_turn : SKSpriteNode?
+    var Robot : SKSpriteNode?
+    var RAM : SKSpriteNode?
+    var button_Start : SKSpriteNode?
     
     override func didMoveToView(view: SKView) {
         background = SKSpriteNode(imageNamed: "background")
@@ -24,12 +27,12 @@ class GameScene: SKScene {
         
         button_pause = SKSpriteNode(imageNamed: "button_Paused")
         button_pause?.size = CGSize(width: size.width * 110/2048, height: size.height * 110/1536)
-        button_pause?.position = CGPoint(x: size.width * 120 / 2048, y: size.height * 1420 / 1536)
+        button_pause?.position = CGPoint(x: size.width * 128 / 2048, y: size.height * 1387 / 1536)
         addChild(button_pause!)
         
         button_tips = SKSpriteNode(imageNamed: "button_Tip")
         button_tips?.size = CGSize(width: size.width * 110/2048, height: size.height * 110/1536)
-        button_tips?.position = CGPoint(x: size.width * 120/2048, y: size.height * 1300/1536)
+        button_tips?.position = CGPoint(x: size.width * 128/2048, y: size.height * 1257/1536)
         addChild(button_tips!)
         
         button_moveforward = SKSpriteNode(imageNamed: "button_moveforward")
@@ -42,6 +45,20 @@ class GameScene: SKScene {
         button_turn?.position = CGPoint(x: size.width * 560/2048, y: size.height * 218/1536)
         addChild(button_turn!)
         
+        Robot = SKSpriteNode(imageNamed: "robot")
+        Robot?.size = CGSize(width: size.width * 225/2048, height: size.height * 356/1536)
+        moveRobotToStart()
+        addChild(Robot!)
+        
+        RAM = SKSpriteNode(imageNamed: "RAM")
+        RAM?.size = CGSize(width: size.width * 238/2048, height: size.height * 81/1536)
+        RAM?.position = CGPoint(x: size.width * 1506/2048, y: size.height * 989/1536)
+        addChild(RAM!)
+        
+        button_Start = SKSpriteNode(imageNamed: "button_Start")
+        button_Start?.size = CGSize(width: size.width * 169/2048, height: size.height * 169/1536)
+        button_Start?.position = CGPoint(x: size.width * 1660/2048, y: size.height * 194/1536)
+        addChild(button_Start!)
         
         
        
@@ -54,4 +71,16 @@ class GameScene: SKScene {
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    
+    
+    
+    
+    
+    func moveRobotToStart()
+    {
+        Robot?.position = CGPoint(x: size.width * 515/2048, y: size.height * 1052/1536)
+    }
+    
+    
 }

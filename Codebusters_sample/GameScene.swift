1b78b14c8ff77aa1e2a3b5cb82527fefa2fb9b19
Alexgo25 <alexgo25616@gmail.com>
2015-04-01 13:01:26 +0300

    touchesEnded_upd

--- Codebusters_sample/GameScene.swift
+++ Codebusters_sample/GameScene.swift
@@ -54,36 +54,43 @@ class GameScene: SKScene {
         button_pause = SKSpriteNode(imageNamed: "button_Paused")
         button_pause?.size = CGSize(width: size.width * 110/2048, height: size.height * 110/1536)
         button_pause?.position = CGPoint(x: size.width * 128 / 2048, y: size.height * 1387 / 1536)
+        button_pause?.name = "pause"
         addChild(button_pause!)
         
         button_tips = SKSpriteNode(imageNamed: "button_Tip")
         button_tips?.size = CGSize(width: size.width * 110/2048, height: size.height * 110/1536)
         button_tips?.position = CGPoint(x: size.width * 128/2048, y: size.height * 1257/1536)
+        button_tips?.name = "tips"
         addChild(button_tips!)
         
         Robot = SKSpriteNode(imageNamed: "robot")
         Robot?.size = CGSize(width: size.width * 225/2048, height: size.height * 356/1536)
+        Robot?.name = "robot"
         moveRobotToStart()
         addChild(Robot!)
         
         RAM = SKSpriteNode(imageNamed: "RAM")
         RAM?.size = CGSize(width: size.width * 238/2048, height: size.height * 81/1536)
         RAM?.position = CGPoint(x: size.width * 1506/2048, y: size.height * 989/1536)
+        RAM?.name = "RAM"
         addChild(RAM!)
         
         button_Start = SKSpriteNode(imageNamed: "button_Start")
         button_Start?.size = CGSize(width: size.width * 169/2048, height: size.height * 169/1536)
         button_Start?.position = CGPoint(x: size.width * 1660/2048, y: size.height * 194/1536)
+        button_Start?.name = "start"
         addChild(button_Start!)
         
         button_moveforward = SKSpriteNode(imageNamed: "button_moveforward")
         button_moveforward?.size = CGSize(width: size.width * 119/2048, height: size.height * 118/1536)
         button_moveforward?.position = CGPoint(x: size.width * 362/2048, y: size.height * 218/1536)
+        button_moveforward?.name = "moveforward"
         addChild(button_moveforward!)
         
         button_turn = SKSpriteNode(imageNamed: "button_Turn")
         button_turn?.size = CGSize(width: size.width * 119/2048, height: size.height * 118/1536)
         button_turn?.position = CGPoint(x: size.width * 560/2048, y: size.height * 218/1536)
+        button_turn?.name = "turn"
         addChild(button_turn!)
         
         
@@ -96,53 +103,6 @@ class GameScene: SKScene {
     
     override func didMoveToView(view: SKView) {
         defaultScene()
-/*background = SKSpriteNode(imageNamed: "background_upd")
-        background?.size = self.size
-        background?.position = CGPoint(x: size.width/2, y: size.height/2)
-        addChild(background!)
-        
-        button_pause = SKSpriteNode(imageNamed: "button_Paused")
-        button_pause?.size = CGSize(width: size.width * 110/2048, height: size.height * 110/1536)
-        button_pause?.position = CGPoint(x: size.width * 128 / 2048, y: size.height * 1387 / 1536)
-        addChild(button_pause!)
-        
-        button_tips = SKSpriteNode(imageNamed: "button_Tip")
-        button_tips?.size = CGSize(width: size.width * 110/2048, height: size.height * 110/1536)
-        button_tips?.position = CGPoint(x: size.width * 128/2048, y: size.height * 1257/1536)
-        addChild(button_tips!)
-        
-        Robot = SKSpriteNode(imageNamed: "robot")
-        Robot?.size = CGSize(width: size.width * 225/2048, height: size.height * 356/1536)
-        moveRobotToStart()
-        addChild(Robot!)
-        
-        RAM = SKSpriteNode(imageNamed: "RAM")
-        RAM?.size = CGSize(width: size.width * 238/2048, height: size.height * 81/1536)
-        RAM?.position = CGPoint(x: size.width * 1506/2048, y: size.height * 989/1536)
-        addChild(RAM!)
-        
-        button_Start = SKSpriteNode(imageNamed: "button_Start")
-        button_Start?.size = CGSize(width: size.width * 169/2048, height: size.height * 169/1536)
-        button_Start?.position = CGPoint(x: size.width * 1660/2048, y: size.height * 194/1536)
-        addChild(button_Start!)
-        
-        button_moveforward = SKSpriteNode(imageNamed: "button_moveforward")
-        button_moveforward?.size = CGSize(width: size.width * 119/2048, height: size.height * 118/1536)
-        button_moveforward?.position = CGPoint(x: size.width * 362/2048, y: size.height * 218/1536)
-        addChild(button_moveforward!)
-        
-        button_turn = SKSpriteNode(imageNamed: "button_Turn")
-        button_turn?.size = CGSize(width: size.width * 119/2048, height: size.height * 118/1536)
-        button_turn?.position = CGPoint(x: size.width * 560/2048, y: size.height * 218/1536)
-        addChild(button_turn!)
-        
-        
-        
-        firstBlockCenter = CGPoint(x: size.width * 879/2048, y: size.height * 193/1536)
-        secondBlockCenter = CGPoint(x: size.width * 1017/2048, y: size.height * 193/1536)
-        thirdBlockCenter = CGPoint(x: size.width * 1155/2048, y: size.height * 193/1536)
-        fourthBlockCenter = CGPoint(x: size.width * 1293/2048, y: size.height * 193/1536)
-       */
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -225,51 +185,24 @@ class GameScene: SKScene {
             if (touchedNode == tempbutton_moveforward ) {
                 
                 tempbutton_moveforward!.position = touchLocation
-                if (detailIsNearTheCenterPosition(firstBlockCenter!, detail: tempbutton_moveforward!))
-                {
-                    firstBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
-                    firstBlockButton!.size = button_moveforward!.size
-                    firstBlockButton!.position = firstBlockCenter!
-                    addChild(firstBlockButton!)
-                    tempbutton_moveforward!.removeFromParent()
-                    tempbutton_moveforward = nil
-                    moves.append("forward")
-                }
-                else if (detailIsNearTheCenterPosition(secondBlockCenter!, detail: tempbutton_moveforward!))
-                {
-                    secondBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
-                    secondBlockButton!.size = button_moveforward!.size
-                    secondBlockButton!.position = secondBlockCenter!
-                    addChild(secondBlockButton!)
-                    tempbutton_moveforward!.removeFromParent()
-                    tempbutton_moveforward = nil
-                    moves.append("forward")
-                }
-                else if (detailIsNearTheCenterPosition(thirdBlockCenter!, detail: tempbutton_moveforward!))
-                {
-                    thirdBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
-                    thirdBlockButton!.size = button_moveforward!.size
-                    thirdBlockButton!.position = thirdBlockCenter!
-                    addChild(thirdBlockButton!)
-                    tempbutton_moveforward!.removeFromParent()
-                    tempbutton_moveforward = nil
-                    moves.append("forward")
-                }
-                else if (detailIsNearTheCenterPosition(fourthBlockCenter!, detail: tempbutton_moveforward!))
-                {
-                    fourthBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
-                    fourthBlockButton!.size = button_moveforward!.size
-                    fourthBlockButton!.position = fourthBlockCenter!
-                    addChild(fourthBlockButton!)
-                    tempbutton_moveforward!.removeFromParent()
-                    tempbutton_moveforward = nil
-                    moves.append("forward")
-                }
                 
             }
             else if (touchedNode == tempbutton_turn)
             {
                 tempbutton_turn!.position = touchLocation
+                            }
+            
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        for touch : AnyObject in touches
+        {
+            let touchLocation = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(touchLocation)
+            
+            if (touchedNode == tempbutton_turn)
+            {
                 if (detailIsNearTheCenterPosition(firstBlockCenter!, detail: tempbutton_turn!))
                 {
                     firstBlockButton = SKSpriteNode(imageNamed: "button_Turn")
@@ -310,27 +243,59 @@ class GameScene: SKScene {
                     tempbutton_turn = nil
                     moves.append("turn")
                 }
-            }
-            
-        }
-    }
-    
-    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
-        for touch : AnyObject in touches
-        {
-            let touchLocation = touch.locationInNode(self)
-            let touchedNode = nodeAtPoint(touchLocation)
-            
-            if (touchedNode == tempbutton_turn)
-            {
+
+                else {
                 tempbutton_turn?.removeFromParent()
                 tempbutton_turn = nil
+                }
             }
 
             if (touchedNode == tempbutton_moveforward)
             {
+                if (detailIsNearTheCenterPosition(firstBlockCenter!, detail: tempbutton_moveforward!))
+                {
+                    firstBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
+                    firstBlockButton!.size = button_moveforward!.size
+                    firstBlockButton!.position = firstBlockCenter!
+                    addChild(firstBlockButton!)
+                    tempbutton_moveforward!.removeFromParent()
+                    tempbutton_moveforward = nil
+                    moves.append("forward")
+                }
+                else if (detailIsNearTheCenterPosition(secondBlockCenter!, detail: tempbutton_moveforward!))
+                {
+                    secondBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
+                    secondBlockButton!.size = button_moveforward!.size
+                    secondBlockButton!.position = secondBlockCenter!
+                    addChild(secondBlockButton!)
+                    tempbutton_moveforward!.removeFromParent()
+                    tempbutton_moveforward = nil
+                    moves.append("forward")
+                }
+                else if (detailIsNearTheCenterPosition(thirdBlockCenter!, detail: tempbutton_moveforward!))
+                {
+                    thirdBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
+                    thirdBlockButton!.size = button_moveforward!.size
+                    thirdBlockButton!.position = thirdBlockCenter!
+                    addChild(thirdBlockButton!)
+                    tempbutton_moveforward!.removeFromParent()
+                    tempbutton_moveforward = nil
+                    moves.append("forward")
+                }
+                else if (detailIsNearTheCenterPosition(fourthBlockCenter!, detail: tempbutton_moveforward!))
+                {
+                    fourthBlockButton = SKSpriteNode(imageNamed: "button_moveforward")
+                    fourthBlockButton!.size = button_moveforward!.size
+                    fourthBlockButton!.position = fourthBlockCenter!
+                    addChild(fourthBlockButton!)
+                    tempbutton_moveforward!.removeFromParent()
+                    tempbutton_moveforward = nil
+                    moves.append("forward")
+                }
+                else {
                 tempbutton_moveforward?.removeFromParent()
                 tempbutton_moveforward = nil
+                }
             }
         }
     }
@@ -353,6 +318,32 @@ class GameScene: SKScene {
         else {
             return false
         }
+    }
+    
+    func createCloneForNode(node : SKSpriteNode)
+    {
+        if (node.name == "moveforward")
+        {
+            if (tempbutton_moveforward == nil) {
+                tempbutton_moveforward = SKSpriteNode(imageNamed: "button_moveforward")
+                tempbutton_moveforward!.size = button_moveforward!.size
+                tempbutton_moveforward!.position = CGPoint(x: size.width * 362/2048, y: size.height * 218/1536)
+                tempbutton_moveforward!.zPosition = 1
+                addChild(tempbutton_moveforward!)
+        }
+        }
+        else if (node.name == "turn")
+        {
+            if (tempbutton_turn == nil) {
+                tempbutton_turn = SKSpriteNode(imageNamed: "button_moveforward")
+                tempbutton_turn!.size = button_moveforward!.size
+                tempbutton_turn!.position = CGPoint(x: size.width * 362/2048, y: size.height * 218/1536)
+                tempbutton_turn!.zPosition = 1
+                addChild(tempbutton_turn!)
+        }
+        
+        
+    }
     }
     
     

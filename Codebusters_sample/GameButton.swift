ac6671e74826b994ebb1cbdec011f107a68995d2
vkuteynikov <vladkuteinikov@gmail.com>
2015-10-11 13:38:22 +0300

    2

--- Codebusters_sample/GameButton.swift
+++ Codebusters_sample/GameButton.swift
@@ -34,7 +34,7 @@ class GameButton: SKSpriteNode {
         gameButtonType = type
         super.init(texture: texture, color: UIColor(), size: texture.size())
         position = getGameButtonPosition(type)
-        zPosition = 2000
+        zPosition = 1001
 
         userInteractionEnabled = true
     }
